3c7e9c03ad9a72f5949f72d7137e775b3aa75aec
lauraborgeess <laura.borgeess@gmail.com>
2022-09-29 18:00:40 -0300

    Códigos até aula 57 - Orientação a Objetos: Sobrecarga de Métodos - Maratona Java DevDojo

---
+++ src/academy/devdojo/maratonajava/javacore/Csobrecargametodos/test/SeriadoTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Csobrecargametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Csobrecargametodos.dominio.Seriado;
+
+public class SeriadoTest01 {
+    public static void main(String[] args) {
+        Seriado seriado = new Seriado();
+        seriado.init("FRIENDS", "Sitcom", 20, "Comedia");
+
+        seriado.imprime();
+
+    }
+}
